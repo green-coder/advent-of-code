2870f15bee4ed138dc57670c9ce3b90da35b52fd
Vincent Cantin <vincent@404.taipei>
2020-12-03 13:57:02 +0800

    day 3

--- src/aoc/day_3.clj
+++ src/aoc/day_3.clj
@@ -5,10 +5,31 @@
   (->> (io/resource "day3.txt")
        io/reader
        line-seq
-       (mapv (fn [line]))))
+       (mapv (fn [line] line))))
+
+(def map-width (-> input (nth 0) count))
+(def map-height (count input))
 
 ;; Part 1
-(->> input)
+(->> (for [y (range (count input))
+           :let [x (mod (* y 3) map-width)]]
+       (get-in input [y x]))
+     (filter #{\#})
+     count)
 
 ;; Part 2
-(->> input)
+(->> (for [[slope-x slope-y] [[1 1]
+                              [3 1]
+                              [5 1]
+                              [7 1]
+                              [1 2]]]
+       (->> (iterate (fn [[x y]]
+                       [(mod (+ x slope-x) map-width) (+ y slope-y)])
+                     [0 0])
+            (take-while (fn [[x y]]
+                          (< y map-height)))
+            (map (fn [[x y]]
+                   (get-in input [y x])))
+            (filter #{\#})
+            count))
+     (apply *))
