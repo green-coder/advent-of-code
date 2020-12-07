20a64136d73e690ab03eb3fb8d05c2a5457e3dfb
Vincent Cantin <vincent@404.taipei>
2020-12-07 14:23:32 +0800

    Added solution for day 7.

---
+++ src/aoc/day_7.clj
@@ -0,0 +1,89 @@
+(ns aoc.day-7
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]
+            [clojure.set :as set]
+            [clojure.edn :as edn]
+            [clojure.data.priority-map :as pm]
+            [clojure.math.combinatorics :as comb]
+            [medley.core :as medley]
+            [aoc.util :as util]))
+
+(defn parse-input [input]
+  (->> input
+       (map (fn [line]
+              (let [[_ out-color right-expr] (re-matches #"(.*) bags contain (.*)." line)
+                    contained-exprs (if (= right-expr "no other bags")
+                                      []
+                                      (str/split right-expr #", "))
+                    in-color (into {}
+                                   (map (fn [contained-expr]
+                                         (let [[_ n in-color] (re-matches #"(\d+) (.*) bags?" contained-expr)]
+                                           [in-color (util/parse-number n)])))
+                                   contained-exprs)]
+                [out-color in-color])))
+       (into {})))
+
+;; Demo input 1
+(def input
+  (->> "light red bags contain 1 bright white bag, 2 muted yellow bags.
+dark orange bags contain 3 bright white bags, 4 muted yellow bags.
+bright white bags contain 1 shiny gold bag.
+muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
+shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
+dark olive bags contain 3 faded blue bags, 4 dotted black bags.
+vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
+faded blue bags contain no other bags.
+dotted black bags contain no other bags."
+       str/split-lines
+       parse-input))
+
+;; Demo input 2
+(def input
+  (->> "shiny gold bags contain 2 dark red bags.
+dark red bags contain 2 dark orange bags.
+dark orange bags contain 2 dark yellow bags.
+dark yellow bags contain 2 dark green bags.
+dark green bags contain 2 dark blue bags.
+dark blue bags contain 2 dark violet bags.
+dark violet bags contain no other bags."
+       str/split-lines
+       parse-input))
+
+;; Real input
+(def input
+  (->> (line-seq (io/reader (io/resource "day7.txt")))
+       parse-input))
+
+(def reverse-input
+  (->> (into []
+             (mapcat (fn [[out ins]]
+                       (map (fn [in] [in out]) (keys ins))))
+             input)
+       (reduce (fn [acc [in out]]
+                 (update acc in conj out))
+               (into {}
+                     (map (juxt identity (constantly #{})))
+                     (keys input)))))
+
+;; Part 1
+(loop [visited #{}
+       frontier ["shiny gold"]]
+  (if (empty? frontier)
+    (dec (count visited))
+    (let [color (peek frontier)
+          visited (conj visited color)
+          frontier (into (pop frontier)
+                         (remove visited)
+                         (reverse-input color))]
+      (recur visited frontier))))
+
+;; Part 2.
+(def solve
+  (memoize (fn [outter-color]
+             (->> (input outter-color)
+                  (transduce (map (fn [[inner-coloc factor]]
+                                    (* (solve inner-coloc) factor)))
+                             +
+                             1)))))
+
+(dec (solve "shiny gold"))
