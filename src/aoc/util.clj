bbc5c0840b81729d6cebf720f4fc74f2cc898b8f
Vincent Cantin <vincent@404.taipei>
2020-12-10 10:10:21 +0800

    Added utility functions.

--- src/aoc/util.clj
+++ src/aoc/util.clj
@@ -1,5 +1,7 @@
 (ns aoc.util
-  (:require [clojure.edn :as edn]
+  (:refer-clojure :exclude [group-by])
+  (:require [clojure.core :as cc]
+            [clojure.edn :as edn]
             [clojure.string :as str]))
 
 (defn parse-number [s]
@@ -28,5 +30,81 @@
 
 #_ (parse-base-n 36 "10") ; [0-9a-zA-Z]*
 
+
 (defmacro first-for [& for-args]
   `(first (for ~@for-args)))
+
+
+(defn comp-> [& args]
+  (apply comp (reverse args)))
+
+#_ ((comp str inc) 17) ; => "18"
+#_ ((comp-> inc str) 17) ; => "18"
+
+
+(defn group-by
+  "Same as clojure.core/group-by, but with some handy new arities which apply
+   custom map & reduce operations to the elements grouped together under the same key."
+  ([kf coll]
+   ;(group-by kf identity conj [] coll)
+   (cc/group-by kf coll))
+  ([kf vf coll]
+   (group-by kf vf conj [] coll))
+  ([kf vf rf coll]
+   (group-by kf vf rf (rf) coll))
+  ([kf vf rf init coll]
+   (persistent!
+    (reduce
+     (fn [ret x]
+       (let [k (kf x)
+             v (vf x)]
+         (assoc! ret k (rf (get ret k init) v))))
+     (transient {}) coll))))
+
+#_ (group-by first [[:a 1] [:a 2] [:b 3] [:a 4] [:b 5]])
+#_ (group-by first second [[:a 1] [:a 2] [:b 3] [:a 4] [:b 5]])
+#_ (group-by first second + [[:a 1] [:a 2] [:b 3] [:a 4] [:b 5]])
+#_ (group-by first second + 10 [[:a 1] [:a 2] [:b 3] [:a 4] [:b 5]])
+
+
+(defn partitionv
+  ([n vec-coll]
+   (partitionv n n vec-coll))
+  ([n step vec-coll]
+   (for [i (range n (inc (count vec-coll)) step)]
+     (subvec vec-coll (- i n) i))))
+
+#_ (partitionv 3 (vec (range 10)))
+#_ (partitionv 3 1 (vec (range 10)))
+
+
+(defn partitionv-all
+  [n vec-coll]
+  (let [coll-size (count vec-coll)]
+    (for [i (range 0 coll-size n)]
+      (subvec vec-coll i (min (+ i n) coll-size)))))
+
+#_ (partitionv-all 3 (vec (range 10)))
+
+
+(defn partition-indexes
+  "Returns pairs of indexes [min sup] to be used with subvec,
+   where min is included and sup is excluded."
+  ([n coll-size]
+   (partition-indexes n n coll-size))
+  ([n step coll-size]
+   (for [i (range n (inc coll-size) step)]
+     [(- i n) i])))
+
+#_ (partition-indexes 3 10)
+#_ (partition-indexes 3 1 10)
+
+
+(defn partition-all-indexes
+  "Returns pairs of indexes [min sup] to be used with subvec,
+   where min is included and sup is excluded."
+  [n coll-size]
+  (for [i (range 0 coll-size n)]
+    [i (min (+ i n) coll-size)]))
+
+#_ (partition-all-indexes 3 10)
