210606d320acad7cbf26ae5a16580022bb800b11
Vincent Cantin <vincent@404.taipei>
2020-12-14 21:53:42 +0800

    Cleaned up the code for day 14.

--- src/aoc/day_14.clj
+++ src/aoc/day_14.clj
@@ -14,61 +14,36 @@
                    [:mem (parse-number addr) (parse-number val)]))))))
 
 
-;; Demo input
-(def input
-  (parse-input "mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X
-mem[8] = 11
-mem[7] = 101
-mem[8] = 0"))
-
-;; Demo input2
-(def input
-  (parse-input "mask = 000000000000000000000000000000X1001X
-mem[42] = 100
-mask = 00000000000000000000000000000000X0XX
-mem[26] = 1"))
-
-;; Real input
 (def input
   (parse-input (slurp (io/resource "day14.txt"))))
 
 ;; Part 1
-(->> (reduce (fn [[n-mask x-mask v-mask memory] [op arg1 arg2]]
-               ;(prn umask x-mask memory)
+(->> (reduce (fn [[x-mask v-mask memory] [op arg1 arg2]]
                (case op
-                 :mask (let [n-mask (->> arg1
-                                         (map {\0 1, \1 1, \X 0})
-                                         (apply str)
-                                         parse-binary)
-                             x-mask (->> arg1
-                                         (map {\0 0, \1 0, \X 1})
-                                         (apply str)
-                                         parse-binary)
-                             v-mask (->> arg1
-                                         (map {\0 0, \1 1, \X 0})
-                                         (apply str)
-                                         parse-binary)]
-                         [n-mask x-mask v-mask memory])
-                 :mem [n-mask x-mask v-mask
-                       (assoc memory arg1
-                                     (bit-or (bit-and arg2 x-mask) v-mask))]))
-             [0 0 0 {}]
+                 :mask [(parse-binary (str/escape arg1 {\1 \0, \X \1}))
+                        (parse-binary (str/escape arg1 {\X \0}))
+                        memory]
+                 :mem [x-mask v-mask
+                       (assoc memory
+                         arg1 (bit-or (bit-and arg2 x-mask) v-mask))]))
+             [0 0 {}]
              input)
      last
      vals
      (reduce +))
+;=> 13496669152158
 
 
 ;; Part 2 - Naive approach
 (defn get-addresses [mask]
   (reduce (fn [acc m-bit] acc
             (case m-bit
-              \0 (mapcat (fn [n]
-                           [(bit-shift-left n 1)])
-                         acc)
-              \1 (mapcat (fn [n]
-                           [(inc (bit-shift-left n 1))])
-                         acc)
+              \0 (map (fn [n]
+                        (bit-shift-left n 1))
+                      acc)
+              \1 (map (fn [n]
+                        (inc (bit-shift-left n 1)))
+                      acc)
               \X (mapcat (fn [n]
                            (let [n (bit-shift-left n 1)]
                              [n (inc n)]))
@@ -81,12 +56,9 @@ mem[26] = 1"))
                  :mask [arg1 memory]
                  :mem [mask (let [n (Long/toBinaryString arg1)
                                   addr-mask (apply str (reverse (map (fn [n m]
-                                                                      (case m
-                                                                        \0 n
-                                                                        \1 \1
-                                                                        \X))
-                                                                    (concat (reverse n) (repeat \0))
-                                                                    (reverse mask))))]
+                                                                       (if (= \0 m) n m))
+                                                                     (concat (reverse n) (repeat \0))
+                                                                     (reverse mask))))]
                               (reduce (fn [acc addr]
                                         (assoc acc addr arg2))
                                       memory
@@ -96,3 +68,4 @@ mem[26] = 1"))
      last
      vals
      (reduce +))
+;=> 3278997609887
