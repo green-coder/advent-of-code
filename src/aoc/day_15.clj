57762818c0718a4f1f88ba781c5335c132d4281d
Vincent Cantin <vincent@404.taipei>
2020-12-17 02:35:44 +0800

    Added solution for day 15,

---
+++ src/aoc/day_15.clj
@@ -0,0 +1,33 @@
+(ns aoc.day-15
+  (:refer-clojure :exclude [group-by])
+  (:require [clojure.string :as str]
+            [aoc.util :refer :all]))
+
+(defn parse-input [input-str]
+  (->> (str/split input-str #",")
+       (mapv parse-number)))
+
+(def input
+  (parse-input "0,3,1,6,7,5"))
+
+;; Part 1
+(defn solve1 [input nb-turns]
+  (loop [turn (inc (count input))
+         last-spoken (last input)
+         mem (into {}
+                   (map-indexed (fn [index n]
+                                  [n (inc index)]))
+                   (butlast input))]
+    (let [n (if (contains? mem last-spoken)
+              (- turn (mem last-spoken) 1)
+              0)]
+      (if (= turn nb-turns)
+        n
+        (recur (inc turn) n (assoc mem last-spoken (dec turn)))))))
+
+(solve1 input 2020)
+;=> 852
+
+;; Part 2
+(solve1 input 30000000)
+;=> 6007666
