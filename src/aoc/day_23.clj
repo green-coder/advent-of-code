d8cf000e2261b4c6afa02af10e906b893756cb79
Vincent Cantin <vincent@404.taipei>
2020-12-23 22:05:23 +0800

    Cleaned up the code, improved memory footprint (and performance)

--- src/aoc/day_23.clj
+++ src/aoc/day_23.clj
@@ -13,91 +13,62 @@
 
 
 ;; Part 1
-(defn remove-cups [cups]
-  (into [(first cups)] (subvec cups 4)))
+(def input-count (count input))
 
-(defn insert-cups [cups index elms]
-  (vec (concat (subvec cups 0 index) elms (subvec cups index))))
+(defn index->label [index]
+  (if (< index input-count)
+    (input index)
+    (inc index)))
 
-(defn rotate-cups [cups]
-  (conj (subvec cups 1) (first cups)))
-
-(defn game-move [cups]
-  (let [current (first cups)
-        pickup-cups (subvec cups 1 4)
-        cups (remove-cups cups)
-        destination (->> current
-                         (iterate (fn [x]
-                                    (inc (mod (- x 2) (count input)))))
-                         (drop 1)
-                         (remove (set pickup-cups))
-                         first)
-        dest-index (->> cups
-                        (keep-indexed (fn [index v]
-                                        (when (= v destination)
-                                          index)))
-                        first)
-        cups (insert-cups cups (inc dest-index) pickup-cups)]
-    (rotate-cups cups)))
-
-;; Some "by hand" data process was happening here ...
-#_ (->> input
-        (iterate game-move)
-        (drop 100)
-        first
-        rotate-cups
-        rotate-cups
-        rotate-cups
-        rotate-cups
-        rotate-cups
-        rotate-cups
-        rotate-cups)
-
-
-;; Part 2
-
-(def nb-cups 1000000)
-(def cups (into input (range 10 (inc nb-cups))))
-(def next-cup-index (into []
-                          (map (fn [x]
-                                 (mod x (count cups))))
-                          (range 1 (inc nb-cups))))
-
-(defn index-of [elm]
-  (if (<= elm (count input))
+(defn label->index [label]
+  (if (<= label input-count)
     (loop [index 0]
-      (if (= (cups index) elm)
+      (if (= (index->label index) label)
         index
         (recur (inc index))))
-    (dec elm)))
+    (dec label)))
 
-(defn fast-game-move [[current-cup-index next-cup-index]]
-  (let [pickup-cup-indexes (->> current-cup-index
-                                (iterate next-cup-index)
-                                (drop 1)
-                                (take 3))
-        pickup-cup-labels (map cups pickup-cup-indexes)
-        dest-label (->> (cups current-cup-index)
-                        (iterate (fn [x]
-                                   (inc (mod (- x 2) (count cups)))))
-                        (drop 1)
-                        (remove (set pickup-cup-labels))
-                        first)
-        dest-index (index-of dest-label)
-        next-cup-index (assoc next-cup-index
-                         ;; set the current's next
-                         current-cup-index (next-cup-index (last pickup-cup-indexes))
-                         ;; set the dest's next
-                         dest-index (index-of (first pickup-cup-labels))
-                         ;; set the last pickup's next
-                         (last pickup-cup-indexes) (next-cup-index dest-index))]
-    [(next-cup-index current-cup-index) next-cup-index]))
+(defn play-game [nb-cups nb-rounds]
+  (let [next-index (into []
+                         (map (fn [index]
+                                (mod (inc index) nb-cups)))
+                         (range nb-cups))
+        lower-label (fn [x]
+                      (-> x (- 2) (mod nb-cups) inc))
+        fast-game-round (fn [[current-index next-index]]
+                          (let [picked-up-indexes (->> current-index
+                                                       (iterate next-index)
+                                                       (drop 1)
+                                                       (take 3))
+                                picked-up-labels (mapv index->label picked-up-indexes)
+                                dest-label (->> (index->label current-index)
+                                                (iterate lower-label)
+                                                (drop 1)
+                                                (remove (set picked-up-labels))
+                                                first)
+                                dest-index (label->index dest-label)
+                                next-index (assoc next-index
+                                             ;; set the current's next
+                                             current-index (next-index (last picked-up-indexes))
+                                             ;; set the dest's next
+                                             dest-index (first picked-up-indexes)
+                                             ;; set the last pickup's next
+                                             (last picked-up-indexes) (next-index dest-index))]
+                            [(next-index current-index) next-index]))]
+    (-> (iterate fast-game-round [0 next-index])
+        (nth nb-rounds))))
 
-(let [[current-cup-index next-cup-index]
-      (->> [0 next-cup-index]
-           (iterate fast-game-move)
-           (drop 10000000)
-           first)]
-  (* (cups (next-cup-index (index-of 1)))
-     (cups (next-cup-index (next-cup-index (index-of 1))))))
+#_(let [[current-index next-index] (play-game input-count 100)]
+    (->> (label->index 1)
+         (iterate next-index)
+         (drop 1)
+         (map index->label)
+         (take-while (complement #{1}))
+         (apply str)))
+;=> "76952348"
+
+;; Part 2
+#_(let [[current-index next-index] (play-game 1000000 10000000)]
+    (* (-> 1 label->index next-index index->label)
+       (-> 1 label->index next-index next-index index->label)))
 ;=> 72772522064
